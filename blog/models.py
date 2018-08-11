24bc45b55425eff20d2c5fa42073a11797530986
root <root@localhost.localdomain>
2018-08-11 13:18:51 +0430

    Post module and comment module created

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(to=User)
+    title = models.CharField(max_length=120)
+    picture = models.ImageField(upload_to='posts/%y/5m/%d')
+    content = models.TextField()
+
+class Comment(models.Model):
+    owner = models.ForeignKey(to=User)
+    email = models.EmailField()
+    text  = models.TextField()
+    for_post = models.ForeignKey(to=Post)
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
-
+from.models import Post,Comment
 # Create your views here.
 
 def index(request):
-    return HttpResponse('index of blog')
+    posts = Post.objects.all()
+    my_post = posts[0]
+    comments = Comment.objects.filter(for_post=my_post)
+    context = {
+        'posts':posts,
+        'comments':comments
+    }
+    return render(request,'index.html',context)
