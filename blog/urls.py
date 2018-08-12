396954e5ebe1019c8738e70ef49ed65cda3dabac
root <root@localhost.localdomain>
2018-08-13 00:26:11 +0430

    added urls for 'about','contactme','categories','singlePost' and 'singleCategory'

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,12 @@ from .import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^about/$',views.about,name='about'),
+    url(r'^contact/$',views.contact,name='contact'),
+    url(r'^categories/$',views.categories_list,name='cats'),
+    url(r'^post/(?P<id>[0-9]{1,3})$',views.post,name='post'),
+    url(r'^category/(?P<id>[0-9]{1,3})$',views.categorypost,name='catposts'),
+
+
 ]
 
