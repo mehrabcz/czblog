e75990f66248c3daedde81fbd217ebb321ed5f0a
root <root@localhost.localdomain>
2018-08-13 00:30:17 +0430

    registered models in admin site

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Post,Comment
+from .models import Post,Comment,ContactMe,Category
+
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(ContactMe)
+admin.site.register(Category)
